dd0f716c91493fd12a4155bdb39752f8acc3c5ca
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-06-25 14:05:03 +0700

    menambahkan method getallemployee

---
+++ MyBackendApp/DAL/EmployeeDAL.cs
@@ -0,0 +1,51 @@
+﻿using MyBackendApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace MyBackendApp.DAL
+{
+    public class EmployeeDAL
+    {
+        private string GetConnStr()
+        {
+            return WebConfigurationManager
+                .ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            using(SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Employees order by EmpName asc";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        lstEmployee.Add(new Employee
+                        {
+                            EmpId = Convert.ToInt32(dr["EmpId"]),
+                            EmpName = dr["EmpName"].ToString(),
+                            Designation = dr["Designation"].ToString(),
+                            Department = dr["Department"].ToString(),
+                            Qualification = dr["Qualification"].ToString(),
+                            BirthDate = Convert.ToDateTime(dr["Birthdate"])
+                        });
+                    }
+                }
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            return lstEmployee;
+        }
+    }
+}
